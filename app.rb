92ffca960c46afc4e31a4d925d330f63fd076442
okorkov <alex.okorkov@gmail.com>
2020-12-16 10:20:10 -0800

    commit tree 2de3f949fdc0a0b134a66c1b75719c15e268edef

--- app.rb
+++ app.rb
@@ -2,4 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do 
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @input_puppy = params
+    @puppy = Puppy.new(@input_puppy)
+    erb :display_puppy
+  end
 end
--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,12 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy 
+
+  attr_accessor :name, :breed, :age
+
+  def initialize(args)
+    @name = args[:name]
+    @breed = args[:breed]
+    @age = args[:age]
+  end
+
+end
